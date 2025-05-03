d590667146348c9e4256b318396e0842772c747e
Federico <federicomoron8@gmail.com>
2025-05-03 10:11:52 -0300

    refactor: minor improvements in register, navigation, shared UI and exp-button

--- src/app/shared/shared-ui.module.ts
+++ src/app/shared/shared-ui.module.ts
@@ -5,6 +5,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -23,6 +24,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatSelectModule,
     MatSidenavModule,
     MatListModule,
+    MatProgressSpinnerModule,
   ],
 })
 export class SharedUiModule {}
+
