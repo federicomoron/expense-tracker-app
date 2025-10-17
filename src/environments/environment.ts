9cb7de5fd8efc3aa8f48dc75c01878cacfdc0ad8
Federico Morón <55101152+federicomoron@users.noreply.github.com>
2025-10-17 10:14:31 -0300

    chore(environment): update API base URL for new backend endpoint (#64)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://spend-track-api.onrender.com/api',
+  apiUrl: 'https://spend-track-api-1.onrender.com/api',
+  // apiUrl: 'http://localhost:8081/api',
+  // apiUrl: 'https://spend-track-api.onrender.com/api',
 };
