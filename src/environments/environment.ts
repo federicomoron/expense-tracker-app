d6ab71b945c037f584f220b24af535b5927f80d4
Federico <federicomoron8@gmail.com>
2025-10-17 13:46:25 -0300

    chore(environment): keep environment.ts pointing to local dev API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://spend-track-api-1.onrender.com/api',
-  // apiUrl: 'http://localhost:8081/api',
-  // apiUrl: 'https://spend-track-api.onrender.com/api',
+  apiUrl: 'http://localhost:8081/api',
+  // apiUrl: 'https://spend-track-api-1.onrender.com/api',
 };
